da98b0dfc1c68ce1149dfffea03fefad142daaf7
alexoreiro <alexis.oreiro@icloud.com>
2020-09-17 20:55:02 +0000

    variables if else while

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 00 to 99
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+int b;
+
+a = 48;
+while (a < 58)
+{
+b = 48;
+while (b < 58)
+{
+putchar(a);
+putchar(b);
+if ((a == 57) && (b == 57))
+{
+break;
+}
+else
+{
+putchar(44);
+putchar(32);
+}
+b++;
+}
+a++;
+}
+putchar('\n');
+
+return (0);
+}
