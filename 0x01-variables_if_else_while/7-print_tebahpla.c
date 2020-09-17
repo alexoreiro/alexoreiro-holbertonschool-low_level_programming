087aedf8bb4373c1446d4b9815223950bfb35883
alexoreiro <alexis.oreiro@icloud.com>
2020-09-17 20:01:53 +0000

    variables if else while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+char a;
+
+for (a = 'z' ; a >= 'a' ; a--)
+{
+putchar(a);
+}
+putchar(10);
+
+return (0);
+}
