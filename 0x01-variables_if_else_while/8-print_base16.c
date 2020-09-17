23082be7dc88d6db5cc593669aca70306e9e9385
alexoreiro <alexis.oreiro@icloud.com>
2020-09-17 20:39:09 +0000

    variables if else while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+a = 48;
+while (a < 58)
+{
+putchar(a);
+a++;
+}
+a = 97;
+while (a < 103)
+{
+putchar(a)
+a++;
+}
+putchar(10);
+
+return (0);
+}
