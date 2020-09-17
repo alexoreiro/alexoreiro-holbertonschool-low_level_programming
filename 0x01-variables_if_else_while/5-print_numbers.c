2840e96eb49b727556e92c68e4087cbbd00c3440
alexoreiro <alexis.oreiro@icloud.com>
2020-09-17 19:32:38 +0000

    variables if else while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+
+a = 0;
+while (a < 10)
+{
+printf("%d", a);
+a++;
+}
+printf("\n");
+
+return (0);
+}
