9864a8777d5c7a7b75e8e7b73cdd2810433d6ee1
alexoreiro <alexis.oreiro@icloud.com>
2020-09-21 21:15:21 +0000

    nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+int print_last_digit(int n)
+{
+n = n % 10;
+if (n < 0)
+{
+n *= -1;
+_putchar(n + '0');
+}
+else
+_putchar(n + '0');
+
+return (n);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+void jack_bauer(void)
+{
+int i, j, k, n;
+
+i = j = k = n = 0;
+while (i < 3)
+{
+while (j < 10)
+{
+while (k < 6)
+{
+while (n < 10)
+{
+_putchar(i + '0');
+_putchar(j + '0');
+_putchar(':');
+_putchar(k + '0');
+_putchar(n + '0');
+_putchar('\n');
+n++;
+}
+k++;
+n = 0;
+}
+j++;
+k = 0;
+if ((i == 2) && (j == 4))
+break;
+}
+i++;
+j = 0;
+}
+}
