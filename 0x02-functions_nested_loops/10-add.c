fe906b04cf3a850c69cf37f612e7b725c8709fad
alexoreiro <alexis.oreiro@icloud.com>
2020-09-22 15:15:49 +0000

    nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @num1: number to be added
+ * @num2: number t be added
+ * Return: sum
+ */
+int add(int num1, int num2)
+{
+int result;
+result = num1 + num2;
+return (result);
+}
