b311518f023d541fd50dfeeb705180418f01e837
alexoreiro <alexis.oreiro@icloud.com>
2020-09-24 18:33:50 +0000

    more functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,18 +1,26 @@
 #include "holberton.h"
 
 /**
- * print_diagonal - prints a diagonal line in terminal
- * @n: number of _ to be added
+ * print_diagonal - prints a straight line in terminal
+ * @n: number of \ to be added
  * Return: Nothing.
  */
 
 void print_diagonal(int n)
 {
-int i;
+int i, j;
 
-for (i = 0; n > 0 && i < n; i++)
+if (n > 0)
 {
+for (i = 0; i < n; i++)
+{
+j = i;
+while (j--)
+_putchar(' ');
 _putchar('\\');
+_putchar('\n');
 }
+}
+else
 _putchar('\n');
 }
