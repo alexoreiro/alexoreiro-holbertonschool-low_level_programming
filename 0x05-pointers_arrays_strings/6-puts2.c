330391dd4cf8e1115b7b3d088372459dedb2e461
alexoreiro <alexis.oreiro@icloud.com>
2020-09-29 16:50:49 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string
+ * @str: string
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+int count;
+char letter;
+
+for (count = 0; str[count] != 0; count++)
+{
+if (count % 2 != 0)
+continue;
+else
+{
+letter = str[count];
+_putchar(letter);
+}
+}
+_putchar('\n');
+}
