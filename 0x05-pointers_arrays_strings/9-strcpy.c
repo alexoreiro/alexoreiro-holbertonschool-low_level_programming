b4b2ec81bfd454f2b24ea8ad36c52aec5615f991
alexoreiro <alexis.oreiro@icloud.com>
2020-09-29 19:40:29 +0000

    pointers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,8 +11,8 @@ char *_strcpy(char *dest, char *src)
 {
 int count;
 
-for (count = 0; src[count] != '\0'; count++)
+for (count = 0; src[count] = '\0'; count++)
 dest[count] = src[count];
 
-return (dest + 1);
+return (dest);
 }
