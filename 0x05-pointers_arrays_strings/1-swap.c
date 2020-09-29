db932292248d753f232e1f8aeaa655cc96493ed5
alexoreiro <alexis.oreiro@icloud.com>
2020-09-29 12:57:23 +0000

    pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 
 /**
- * main - swap the values of two integers.
- *
+ * swap_int - swap the values of two integers.
+ * @a numbers to be swaped with b
+ * @b numbers to be swaped with a
  * Return: Always 0.
  */
 void swap_int(int *a, int *b)
