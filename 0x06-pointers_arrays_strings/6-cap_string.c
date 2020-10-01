56b3e1f737871735295fedae76a6f1c11140f3f5
alexoreiro <alexis.oreiro@icloud.com>
2020-10-01 17:05:54 +0000

    0x06 more pointers

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitilizes all words of a string
+ * @s: pointer to a string
+ * Return: string in uppercase
+ */
+char *cap_string(char *s)
+{
+int i;
+
+i = 0;
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] -= 32;
+for (i = 1; s[i] != '\0'; i++)
+{
+if ((s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t' ||
+s[i - 1] == '\n') && s[i] >= 'a' && s[i] <= 'z')
+s[i] -= 32;
+}
+
+return (s);
+}
