f899d33992fe8dc03e808d94541c471779a1ed19
alexoreiro <alexis.oreiro@icloud.com>
2020-10-01 16:27:28 +0000

    0x06 more pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to be appended by source string
+ * @src: string to be appended to the string at destination
+ * Return: pointer to string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i] = src[j];
+i++;
+}
+i++;
+dest[i] = '\0';
+return (dest);
+}
