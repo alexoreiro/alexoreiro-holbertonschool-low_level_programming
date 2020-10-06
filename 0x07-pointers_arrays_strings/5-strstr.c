0b1356d707de06f201d25a2da6581bc4310a893c
alexoreiro <alexis.oreiro@icloud.com>
2020-10-06 01:59:07 +0000

    0x07 pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#define NULL 0
+/**
+ * _strstr - locates a substring
+ * @haystack: string to be scanned
+ * @needle: string to be located in haystack
+ * Return: a pointer to the beginning of the located substring needle in the
+ * string haystack; NULL if no such substring is found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+char *start_match, *i;
+
+while (*haystack != '\0' && *needle != '\0')
+{
+start_match = haystack;
+i = needle;
+while (*i == *haystack && *i != '\0'
+&& *haystack != '\0')
+{
+i++;
+haystack++;
+}
+if (*i == '\0')
+return (start_match);
+haystack = start_match + 1;
+}
+return (NULL);
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square
+ * matrix of integers
+ * @a: array
+ * @size: size of
+ * Return: Nothing
+ */
+void print_diagsums(int *a, int size)
+{
+int i, j, sum1, sum2, *copy1, *copy2;
+
+sum1 = sum2 = 0;
+copy1 = copy2 = a;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (i == j)
+sum1 += *copy1;
+copy1++;
+}
+for (j = 0; j < size; j++)
+{
+if (j == size - 1 - i)
+sum2 += *copy2;
+copy2++;
+}
+}
+printf("%d, %d\n", sum1, sum2);
+}
