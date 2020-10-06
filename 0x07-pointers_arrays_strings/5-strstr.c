591f01d62c07eefe2cb5b860ed4b621890a7f984
alexoreiro <alexis.oreiro@icloud.com>
2020-10-06 02:06:43 +0000

    0x07 pointers

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,29 +1,34 @@
 #include "holberton.h"
-#define NULL 0
+
 /**
- * _strstr - locates a substring
- * @haystack: string to be scanned
- * @needle: string to be located in haystack
- * Return: a pointer to the beginning of the located substring needle in the
- * string haystack; NULL if no such substring is found
+ * _strstr - a function that locates a character in a string
+ * @haystack: pointer
+ * @needle: char received
+ * Return:the pointer to char or 0 if dont have coincidence
  */
+
 char *_strstr(char *haystack, char *needle)
 {
-char *start_match, *i;
 
-while (*haystack != '\0' && *needle != '\0')
+int i = 0, j = 0;
+
+while (haystack[i])
 {
-start_match = haystack;
-i = needle;
-while (*i == *haystack && *i != '\0'
-&& *haystack != '\0')
+while (needle[j] && (haystack[i] == needle[0]))
+{
+if (haystack[i + j] == needle[j])
+j++;
+else
+break;
+}
+if (needle[j])
 {
 i++;
-haystack++;
+j = 0;
 }
-if (*i == '\0')
-return (start_match);
-haystack = start_match + 1;
+else
+return (haystack + i);
 }
-return (NULL);
+
+return ('\0');
 }
