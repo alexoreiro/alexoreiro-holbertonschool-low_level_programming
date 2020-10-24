5cfb5a39fc1af3a47998ec2a7b8ef2a50af717f1
alexoreiro <alexis.oreiro@icloud.com>
2020-10-24 16:19:04 +0000

    Variadic Functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,23 +1,19 @@
+#ifndef VARIADIC
+#define VARIADIC
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
-#ifndef VAR_FUNC_H
-#define VAR_FUNC_H
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-int _putchar(char c);
-
-/**
- * struct types - Struct for data types and function pointers
- * @type: The data type to be specified by print_all
- * @f: The corresponding function pointer for each data type
- *
- * Description: Data types specified by print_all have corresponding functions
- */
-typedef struct types
-{
-  char *type;
-  void (*f)();
-} types_t;
 void print_all(const char * const format, ...);
+
+typedef struct pter
+{
+  char *test;
+  void (*printer)();
+} pt;
+
 #endif
