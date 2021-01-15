a8818f460fd31c9cae13fa41dc6b1917b2589e2c
alexoreiro <alexis.oreiro@icloud.com>
2021-01-15 22:21:01 +0000

    crack code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *main - generates a password
+ *
+ *Return: 0 on success
+ **/
+
+int main(void)
+{
+int i = 0;
+int contador = 0;
+int letra;
+char pass[100];
+int resto;
+
+srand(time(NULL));
+
+while (contador < 2645)
+{
+letra = (rand() % 25) + 97;
+pass[i] = letra;
+contador = contador + pass[i];
+i++;
+}
+resto = 2772 - contador;
+pass[i++] = resto;
+pass[i++] = '\0';
+printf("%s\n", pass);
+return (0);
+}
---
+++ 0x05-pointers_arrays_strings/hack.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ *main - generates a password
+ *
+ *Return: 0 on success
+ **/
+
+int main(void)
+{
+int i = 0;
+int contador = 2772;
+int letra;
+char pass[100];
+
+srand(time(NULL));
+
+while (contador != 0)
+{
+letra = rand() % 127;
+if (letra > 32)
+{
+pass[i] = letra;
+i++;
+contador = contador - pass[i];
+}
+}
+pass[i] = '\0';
+printf("%s\n", pass);
+return (0);
+}
