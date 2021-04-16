12ddee0dba6257ffe7a5ee2f4d59e2e9ed7cd45a
alexoreiro <alexis.oreiro@hotmail.com>
2021-04-16 20:33:04 +0000

    compares 2 strings

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+ *aux2 - auxiliary function
+ *@s1: pointer of string 1
+ *@s2: pointer of string 2
+ *Return: pointer
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+  if (*s1 == *s2)
+    return (wildcmp(s1, s2));
+  if (*s2 == '\0')
+    return (aux2(s1 + 1, s2));
+  if (*(s2 + 1) == '\0')
+    return (1);
+  s2++;
+  if (*s2 + 1 == *s1)
+    return (wildcmp(s1, s2 + 1));
+  else if ((*s2 + 1) == '*')
+    return (aux2(s1, s2 + 1));
+  else if (*(s2 + 1) != '*' && *(s2 + 1) != '\0')
+    return (aux2(s1 + 1, s2));
+  if (*s1 == '\0')
+    return (wildcmp(s1, s2));
+  return (aux2(s1 + 1, s2));
+}
+
+/**
+ *wildcmp -  compares two strings
+ *@s1: string 1
+ *@s2: string 2
+ *Return: 1 if equals otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+  if (*s2 == '*')
+    return (aux2(s1, s2));
+  if (*s2 == '\0' && *s1 != '\0')
+    return (0);
+  if (*s1 == '\0' || *s2 == '\0')
+    {
+      if (*s1 == '\0' && *s2 == '\0')
+	return (1);
+      return (0);
+    }
+  if (*s1 == *s2)
+    return (wildcmp(s1 + 1, s2 + 1));
+  return (0);
+}
